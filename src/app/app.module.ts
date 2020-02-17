b696f7822a5af625ed0bda6d8e7381315cf13e1e
zonecc <bkevery@gmail.com>
2020-02-17 12:05:24 +0300

    created header and search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UpcomingListComponent } from './components/movies/upcoming-list/upcoming-list.component';
+import { MovieDetailComponent } from './components/movies/movie-detail/movie-detail.component';
+import { SearchBarComponent } from './components/header/search-bar/search-bar.component';
+import { NavigationComponent } from './components/header/navigation/navigation.component';
+import { MovieListItemComponent } from './components/movies/upcoming-list/movie-list-item/movie-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UpcomingListComponent,
+    MovieDetailComponent,
+    SearchBarComponent,
+    NavigationComponent,
+    MovieListItemComponent
   ],
   imports: [
     BrowserModule,
