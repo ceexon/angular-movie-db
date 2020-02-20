6cb8cc091b3cf2f3b018b7d5d4498032a68048d4
zonecc <bkevery@gmail.com>
2020-02-20 03:00:37 +0300

    remove favorited and push fixes

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -20,29 +20,7 @@ export class MovieService {
   apiKey = environment.apiKey;
   baseImg = environment.baseImageURL;
 
-  private favourites: Movie[] | MovieDetailModel[] | any[] =  [
-    {
-      popularity: 279.237,
-      vote_count: 201,
-      video: false,
-      poster_path: '/aQvJ5WPzZgYVDrxLX4R6cLJCEaQ.jpg',
-      id: 454626,
-      adult: false,
-      backdrop_path: '/tCUcf3oNWMW8kwAj3WC6CvIN5ah.jpg',
-      original_language: 'en',
-      original_title: 'Sonic the Hedgehog',
-      genre_ids: [28, 12, 35, 878, 10751],
-      title: 'Sonic the Hedgehog',
-      vote_average: 7.2,
-      overview: 'Based on the global blockbuster video game franchise from Sega, Sonic the Hedgehog tells the ' +
-        'story of the world’s speediest hedgehog as he embraces his new home on Earth. In this live-action ' +
-        'adventure comedy, Sonic and his new best friend team up to defend the planet from the evil genius Dr. ' +
-        'Robotnik and his plans for world domination.',
-      release_date: '2020-02-12',
-      favorite: true
-    }
-  ];
-  // private favourites: Movie[] | MovieDetailModel[] | any[] = this.movies.filter(m => m.favorite === true );
+  private favourites: Movie[] | MovieDetailModel[] | any[] = this.movies.filter(m => m.favorite === true );
   constructor(private http: HttpClient) {
   }
 
